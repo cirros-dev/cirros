6b57afbdfc3b5a7b7b07a4c968c18887d33cff6f
Scott Moser <smoser@ubuntu.com>
2016-07-22 11:22:37 -0400

    bin/build-release: add human output format to logevent messages.
    We would see something like:
      Thu, 21 Jul 2016 19:06:21 +0000 finished [10892s]
    Now show something like:
      Thu, 21 Jul 2016 19:06:21 +0000 finished. Took 3h 1m 32s. [10892s]

--- bin/common-functions.sh
+++ bin/common-functions.sh
@@ -23,4 +23,45 @@ dl() {
         { t=$?; rm -f "$tfile"; return $t; }
 }
 
+sec2human() {
+    local delta=$1 fmt="${2:-full}" year day hour min sec rem=0
+    rem=$delta
+    year=$(($rem/(365*24*60*60))) || return
+    rem=$(($rem-($year*365*24*60*60)))
+    day=$(($rem/(24*60*60)))
+    rem=$(($rem-($day*24*60*60)))
+    hour=$(($rem/(60*60)))
+    rem=$(($rem-($hour*60*60)))
+    min=$(($rem/(60)))
+    rem=$(($rem-($min*60)))
+    sec=$rem
+    local t="" unit short long
+    local full="" tfull="" brev="" tbrev=""
+    for t in "$year y year" "$day d day" "$hour h hour" \
+            "$min m minute" "$sec s second"; do
+        set -- $t
+        unit=$1; short=$2; long=$3
+        [ "$1" = "1" ] || long="${long}s"
+        full="$full $unit $long"
+        brev="$brev $unit$short"
+        if [ "$unit" != "0" ] || [ -n "$tfull" ]; then
+            tfull="$tfull $unit $long"
+            tbrev="$tbrev $unit$short"
+        fi
+    done
+    tfull=${tfull# }
+    full=${full# }
+    tbrev=${tbrev# }
+    brev=${brev# }
+    case "$fmt" in
+        full) _RET="$full";;    # full words with leading
+        tfull) _RET="$tfull";;  # full words, no leading 0
+        short) _RET="$brev";;   # abbreviated with leading
+        tshort) _RET="$tbrev";; # abbreviated no leading 0
+        *) echo "bad format '$fmt'"; return 1;;
+    esac
+    return
+}
+
+
 # vi: tabstop=4 noexpandtab
